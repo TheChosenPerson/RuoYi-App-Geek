8d6f36192e1f2caa22a93d3f9e7185971225c6d1
D <3066417822@qq.com>
2023-08-16 20:40:00 +0800

    修复h5平台的问题，目前h5平台只能是main.js,暂时不支持main.ts,正在想办法

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 
 export default defineConfig(() => {
     return {
+        base: './',
         build: {
             minify: true,
+            outDir: 'dist',
         },
         server: {
             port: '3002'
