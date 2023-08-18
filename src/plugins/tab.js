c7193fda412419ce2a6423933f681c912774e962
D <3066417822@qq.com>
2023-08-18 12:00:56 +0800

    tab改成promise版本

--- src/plugins/tab.js
+++ src/plugins/tab.js
@@ -1,30 +1,54 @@
 export default {
   // 关闭所有页面，打开到应用内的某个页面
   reLaunch(url) {
-    return uni.reLaunch({
-      url: url
-    })
+    return new Promise((resolve, reject) => {
+      uni.reLaunch({
+        url: url,
+        success: resolve,
+        fail: reject
+      });
+    });
   },
-  // 跳转到tabBar页面，并关闭其他所有非tabBar页面
+
+  // 跳转到 tabBar 页面，并关闭其他所有非 tabBar 页面
   switchTab(url) {
-    return uni.switchTab({
-      url: url
-    })
-  },
+    return new Promise((resolve, reject) => {
+      uni.switchTab({
+        url: url,
+        success: resolve,
+        fail: reject
+      });
+    });
+  }
+  ,
   // 关闭当前页面，跳转到应用内的某个页面
   redirectTo(url) {
-    return uni.redirectTo({
-      url: url
-    })
+    return new Promise((resolve, reject) => {
+      uni.redirectTo({
+        url: url,
+        success: resolve,
+        fail: reject
+      });
+    });
   },
   // 保留当前页面，跳转到应用内的某个页面
   navigateTo(url) {
-    return uni.navigateTo({
-      url: url
-    })
+    return new Promise((resolve, reject) => {
+      uni.navigateTo({
+        url: url,
+        success: resolve,
+        fail: reject
+      });
+    });
   },
+
   // 关闭当前页面，返回上一页面或多级页面
   navigateBack() {
-    return uni.navigateBack()
-  }
+    return new Promise((resolve, reject) => {
+      uni.navigateBack({
+        success: resolve,
+        fail: reject
+      });
+    });
+  },
 }
